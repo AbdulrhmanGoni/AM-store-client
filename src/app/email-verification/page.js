3669fc3d5b41f19812b0c07f8276e15567abda3e
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2023-11-21 01:19:21 +0300

    Enhance email verification page
    - Make verification field thows an error as tooltip to tell the user if he enters
    invalid input.
    Add two image to use them as illustraion images.
    
    - Handle two error cases, "code expired" and "too many requests";
    an error message will appears with an illustration image to descripe the case
    based on the response of the request of `submitCode` function, if returns
    an error with the status code `408`, that is means "time out" ("code expired" case),
    And if the status code is `429`,  that is means the case "too many requests".
    
    - Add two illustration images to `/public` folder for displaying in "time out" case
    and "too many requests" case.

--- src/app/email-verification/page.js
+++ src/app/email-verification/page.js
@@ -4,51 +4,59 @@ import { Box, Button, TextField, Typography } from '@mui/material'
 import { useDispatch, useSelector } from 'react-redux'
 import customFetch from '@/functions/customFetch';
 import { useSpeedMessage } from '@/hooks/useSpeedMessage';
-import { ErrorThrower, LoadingPage } from '@abdulrhmangoni/am-store-library';
-import { useRef } from 'react';
+import { ErrorThrower, LoadingPage, AlertTooltip } from '@abdulrhmangoni/am-store-library';
 import { useRouter } from 'next/navigation';
 import { setEmailAsVerified_localy } from '@/dataBase/userData_slice';
+import { OpenInBrowser } from '@mui/icons-material';
 
 export default function Page() {
 
-    const [currentState, setCurrentState] = useState({ state: "", message: "" });
-    const [isLodading, setIsLodading] = useState(false);
+    const expiredCode = 408;
+    const tooManyRequestsCode = 429;
     const userData = useSelector(state => state.userData);
-    const codeInputRef = useRef();
     const { message } = useSpeedMessage();
     const dispatch = useDispatch();
     const { back } = useRouter();
+    const [currentState, setCurrentState] = useState({ status: "", message: "" });
+    const [isLodading, setIsLodading] = useState(false);
+    const [codeFieldStatus, setCodeFieldStatus] = useState({ isError: false, message: "", value: "" });
 
 
-    function submitCode() {
-        const verificationCode = +codeInputRef.current?.value;
-        if (verificationCode) {
-            setIsLodading(true);
-            customFetch("email-verification", "POST", { verificationCode, userEmail: userData?.userEmail })
-                .then((res) => {
-                    if (res.ok) setCurrentState({ state: "verified successfully" });
-                    else message(res.message);
-                })
-                .catch(({ response }) => {
-                    setCurrentState({ state: "verifying failed", message: response?.data.message });
-                })
-                .finally(() => setIsLodading(false));
-        }
+    function submitCode(verificationCode) {
+        setIsLodading(true);
+        customFetch("email-verification", "POST", { verificationCode, userEmail: userData?.userEmail })
+            .then((res) => {
+                if (res.ok) {
+                    setCurrentState({ status: "Verified successfully" });
+                    dispatch(setEmailAsVerified_localy());
+                }
+                else message(res.message);
+            })
+            .catch(({ response }) => {
+                if (response?.status == expiredCode) {
+                    setCurrentState({ status: expiredCode, message: response?.data.message });
+                }
+                else setCurrentState({ status: "Verifying failed", message: response?.data.message });
+            })
+            .finally(() => setIsLodading(false));
     }
 
     useEffect(() => {
         if (userData && !userData.hisEmailVerified) {
             setIsLodading(true);
             customFetch("email-verification")
-                .catch(() => { setCurrentState({ state: "Email didn't send" }) })
                 .then((res) => {
-                    if (res.ok) {
-                        setCurrentState({ state: "Email sent" });
-                        dispatch(setEmailAsVerified_localy());
-                    } else {
+                    if (res.ok) setCurrentState({ status: "Email sent" })
+                    else {
                         message(res.message, "info");
                         back();
                     }
+                })
+                .catch((err) => {
+                    if (err.response?.status == tooManyRequestsCode) {
+                        setCurrentState({ status: tooManyRequestsCode, message: err.response?.statusText })
+                    }
+                    else setCurrentState({ status: "Email didn't send" })
                 })
                 .finally(() => setIsLodading(false));
         } else if (userData) {
@@ -58,7 +66,7 @@ export default function Page() {
     }, [])
 
     return isLodading ? <LoadingPage />
-        : currentState.state === "Email sent" ? (
+        : currentState.status === "Email sent" ? (
             <ErrorThrower
                 title='Verify your email now !'
                 customIllustrator={
@@ -70,25 +78,42 @@ export default function Page() {
                 }
                 hideAlertMsg
             >
-                <Box component="form" mb={2}>
-                    <TextField
-                        variant="filled"
-                        placeholder='000000'
-                        required
-                        title='Please add verification code here'
-                        disabled={isLodading}
-                        onChange={() => {
-                            const verificationCode = +codeInputRef.current?.value
-                            if (typeof verificationCode === "number" && verificationCode > 99999) {
-                                submitCode();
-                            }
-                        }}
-                        type="text"
-                        size='small'
-                        inputRef={codeInputRef}
-                        inputProps={{ sx: { p: "12px 8px 8px 16px", letterSpacing: 8 } }}
-                        sx={{ width: "126px" }}
-                    />
+                <Box
+                    onSubmit={(e) => e.preventDefault()}
+                    component="form"
+                    mb={2}
+                >
+                    <AlertTooltip
+                        title={codeFieldStatus.message}
+                        type='error'
+                        tooltipProps={{ open: codeFieldStatus.isError }}
+                        action={() => { }}
+                        actionIcon={<OpenInBrowser />}
+                    >
+                        <TextField
+                            variant="filled"
+                            placeholder='000000'
+                            required
+                            value={codeFieldStatus.value}
+                            onChange={({ target: { value } }) => {
+                                const isNumber = !isNaN(+value);
+                                const validLength = value.length === 6;
+                                setCodeFieldStatus({
+                                    isError: !isNumber,
+                                    message: isNumber ? "" : "The code should consist of six numbers",
+                                    value: value.slice(0, 6)
+                                });
+                                if (isNumber && validLength) { submitCode(+value) }
+                            }}
+                            autoFocus
+                            suppressContentEditableWarning={false}
+                            type="text"
+                            size='small'
+                            error={codeFieldStatus.isError}
+                            inputProps={{ sx: { p: "12px 8px 8px 16px", letterSpacing: 8 } }}
+                            sx={{ width: "126px" }}
+                        />
+                    </AlertTooltip>
                 </Box>
                 <Typography sx={{ fontSize: "1.20rem" }}>
                     We sent a verification mail to your account {userData?.userEmail},
@@ -96,44 +121,45 @@ export default function Page() {
                 </Typography>
             </ErrorThrower>
         )
-            : currentState.state === "Email didn't send" ? (
+            : currentState.status === "Verified successfully" ? (
                 <ErrorThrower
-                    title='Ops! Something wrong happends'
+                    title="Coungrates! your email verified successfully"
                     customIllustrator={
                         <Box
                             component="img"
-                            src='/failed.svg'
-                            alt='Illustrator image of failed sending verification mail'
+                            src='/email-verified.png'
+                            alt='Illustrator image of email verified successfully'
                         />
                     }
-                    message='Maybe the problem in your network or in you email email'
-                    withRefreshButton
+                    hideAlertMsg
                 >
-                    <Typography>
-                        Something wrong happends while we try to send the verification code to your email.
-                    </Typography>
+                    <Button onClick={() => { back() }} size='small' variant="contained">
+                        Back
+                    </Button>
                 </ErrorThrower>
             )
-                : currentState.state === "verified successfully" ? (
+                : currentState.status === "Email didn't send" ? (
                     <ErrorThrower
-                        title="Coungrates! your email verified successfully"
+                        title='Ops! Something wrong happends'
                         customIllustrator={
                             <Box
                                 component="img"
-                                src='/email-verified.png'
-                                alt='Illustrator image of email verified successfully'
+                                src='/failed.svg'
+                                alt='Illustrator image of failed sending verification mail'
                             />
                         }
-                        hideAlertMsg
+                        message='Maybe the problem in your network or in you email'
+                        withRefreshButton
                     >
-                        <Button onClick={() => { back() }} size='small' variant="contained">
-                            Back
-                        </Button>
+                        <Typography>
+                            Something wrong happends while we try to send the verification code to your email.
+                        </Typography>
                     </ErrorThrower>
                 )
-                    : currentState.state === "verifying failed" ? (
+                    : currentState.status === "Verifying failed" ? (
                         <ErrorThrower
-                            title={currentState.message}
+                            title={currentState.status}
+                            message={currentState.message}
                             customIllustrator={
                                 <Box
                                     component="img"
@@ -141,11 +167,43 @@ export default function Page() {
                                     alt='Illustrator image of failed email verification'
                                 />
                             }
-                            hideAlertMsg
                         >
                             <Button onClick={() => location.reload()} size='small' variant="contained">
                                 try again
                             </Button>
                         </ErrorThrower>
-                    ) : null;
+                    )
+                        : currentState.status == tooManyRequestsCode ? (
+                            <ErrorThrower
+                                title="Too many verification requests!"
+                                customIllustrator={
+                                    <Box
+                                        component="img"
+                                        src='/too-many-requests.svg'
+                                        alt='Illustrator image of too many requests'
+                                    />
+                                }
+                                hideAlertMsg
+                            >
+                                <Typography>
+                                    You are trying to get verification codes multible times without send them to verify your email
+                                    Come back later and try again
+                                </Typography>
+                            </ErrorThrower>
+                        )
+                            : currentState.status === expiredCode ? (
+                                <ErrorThrower
+                                    title="Verification code expired!"
+                                    message={currentState.message}
+                                    withRefreshButton
+                                    customIllustrator={
+                                        <Box
+                                            component="img"
+                                            src='/timeout.svg'
+                                            alt='Illustrator image of expired code'
+                                        />
+                                    }
+                                ></ErrorThrower>
+                            )
+                                : null;
 }

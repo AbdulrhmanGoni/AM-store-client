9d0d14c9b4fa1a4a2273f6487651b7ca33c9cab7
Abdulrhman <abdulrhmangoni@gmail.com>
2024-01-01 15:12:22 +0300

    Refactor `SendFeedbackForm` component
    - Delete `sendFeedback.js` function and use `customFetch` directly in
    `SendFeedbackForm`
    
    - Replace `userEmail` in the payload of feedback request with `userId`
    
    - Convert `IconButton` conponent that closes the form to normal `Button`
    conponent and move it into the side of the button of sending the form
    
    - Add loading state to the component
    
    - Replace `Grid` component to build the ui of the form with `Box` component
    and flex box feature of CSS

--- src/components/SendFeedbackForm.js
+++ src/components/SendFeedbackForm.js
@@ -1,18 +1,23 @@
 "use client"
-import { Grid, TextField, IconButton, Paper, Button } from '@mui/material'
+import { useMediaQuery, TextField, Button, Paper, Box } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
 import OverlayBg from './OverlayBg'
-import { Bar } from './locationRegistring/LocationsManegement'
-import { Close } from '@mui/icons-material'
-import sendFeedback from '../functions/sendFeedback'
+import { Close, Send } from '@mui/icons-material'
 import { useSelector } from 'react-redux'
 import { useSpeedMessage } from '@/hooks/useSpeedMessage'
 import { P } from "@abdulrhmangoni/am-store-library";
+import { useState } from 'react'
+import pagesSpaces from '@/CONSTANT/pagesSpaces'
+import customFetch from '@/functions/customFetch'
 
 
 export default function SendFeedbackForm({ open, close }) {
 
-    const userEmail = useSelector(state => state.userData?.userEmail);
+    const userId = useSelector(state => state.userData?._id);
     const { message } = useSpeedMessage();
+    const [loading, setLoading] = useState(false);
+    const smallScreen = useMediaQuery("(max-width: 500px)");
+    const inputsSize = smallScreen ? "small" : "medium"
 
     async function sendEmail(ev) {
         ev.preventDefault();
@@ -20,45 +25,71 @@ export default function SendFeedbackForm({ open, close }) {
         const subject = form.get("subject");
         const body = form.get("body");
         if (subject && body) {
-            sendFeedback({ userEmail, subject, body })
-                .catch((() => message("There is unexpected error happeneds")))
-                .then((({ ok, message: msg }) => {
-                    ok && message(msg, "success"); close();
-                    !ok && message(msg, "error");
-                }));
+            setLoading(true);
+            customFetch(`/feedbacks`, "POST", { userId, subject, body })
+                .then(((res) => {
+                    message(res.message, "success");
+                    close();
+                }))
+                .catch(((error) => {
+                    console.log(error)
+                    const errorMessage = error.response?.data?.message
+                    message(errorMessage || "There is unexpected error happeneds")
+                }))
+                .finally(() => setLoading(false))
         }
     }
 
     return (open &&
-        <OverlayBg open={open}>
+        <OverlayBg>
             <Paper
-                sx={{
-                    p: 2,
-                    width: { xs: "300px", sm: "450px", md: "700px" }
-                }}
                 component="form"
                 onSubmit={sendEmail}
                 elevation={2}
+                className='flex-column j-start'
+                sx={{
+                    p: pagesSpaces,
+                    width: { xs: "96%", sm: "520px", md: "700px" }
+                }}
             >
-                <Bar dividerBotton sx={{ p: "0px 0px 8px" }}>
-                    <P variant='h6'>
-                        Tell us your feedback
-                    </P>
-                    <IconButton onClick={() => close()}>
-                        <Close />
-                    </IconButton>
-                </Bar>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField label="Subject" name='subject' fullWidth />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField fullWidth label="Body" name='body' type='textarea' multiline minRows={4} />
-                    </Grid>
-                    <Grid sx={{ display: "flex", flexDirection: "row-reverse" }} item xs={12}>
-                        <Button size='small' type='submit' variant='contained'>Send</Button>
-                    </Grid>
-                </Grid>
+                <P variant='h6' textAlign="start" ml="3px" mb={pagesSpaces}>Tell us your Feedback</P>
+                <Box className="flex-column gap2">
+                    <TextField
+                        label="Subject"
+                        name='subject'
+                        size={inputsSize}
+                        fullWidth
+                    />
+                    <TextField
+                        fullWidth
+                        label="Body"
+                        name='body'
+                        type='textarea'
+                        multiline
+                        size={inputsSize}
+                        minRows={4}
+                    />
+                    <Box sx={{ display: "flex", flexDirection: "row-reverse", gap: pagesSpaces }}>
+                        <LoadingButton
+                            size='small'
+                            type='submit'
+                            variant='contained'
+                            loading={loading}
+                            loadingPosition='end'
+                            endIcon={<Send />}
+                        >
+                            Send
+                        </LoadingButton>
+                        <Button
+                            size='small'
+                            variant='contained'
+                            onClick={() => close()}
+                            endIcon={<Close />}
+                        >
+                            Cancel
+                        </Button>
+                    </Box>
+                </Box>
             </Paper>
         </OverlayBg>
     )

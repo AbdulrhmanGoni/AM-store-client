0afcd6402bd6cce684a1373d9fb6d9b4f2bf2bc8
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-03-05 18:18:16 +0300

    Use the env variable `SERVER_HOST` in production mode
    - Use the env variable `SERVER_HOST` in production mode to create
    the URL of the api (AM Store server) in `hostName` file.

--- src/CONSTANTS/hostName.js
+++ src/CONSTANTS/hostName.js
@@ -1,1 +1,7 @@
-export const host = "http://localhost:7000/api";
+let hostName = "http://localhost:7000";
+
+if (process.env.REACT_APP_NODE_ENV === "production") {
+    hostName = process.env.REACT_APP_SERVER_HOST
+}
+
+export const host = `${hostName}/api`;

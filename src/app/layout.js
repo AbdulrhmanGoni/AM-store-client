6ec8e2f0b76098e5baa6cf86b5821460ab9e0eb1
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-03-08 00:13:12 +0300

    Import `global.css` file after all components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
 "use client"
-import "@abdulrhmangoni/am-store-library/dist/cjs/global.css"
 import { CustomThemeProvider } from "@abdulrhmangoni/am-store-library"
 import { SnackbarProvider } from 'notistack';
 import { CssBaseline } from "@mui/material";
 import { Provider } from "react-redux";
 import dataCenter from "@/state-management/dataCenter"
 import AppWrapper from "./AppWrapper";
+import "@abdulrhmangoni/am-store-library/dist/cjs/global.css"
 
 
 export default function RootLayout({ children }) {
